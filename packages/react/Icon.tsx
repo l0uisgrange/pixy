975b73aead15cc1d25d5b19dfeee4f4e01df3c12
Louis <70532216+l0uisgrange@users.noreply.github.com>
2025-08-14 08:58:39 +0200

    fixed default icon size

--- packages/react/Icon.tsx
+++ packages/react/Icon.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import * as React from 'react';
 
 export default function Icon({ children, ...props }: React.ComponentProps<'svg'>) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
+            stroke="currentColor"
+            fill="none"
             {...props}
         >
+            <style>{`
+                svg * {
+                    stroke: inherit;
+                }
+            `}</style>
             {children}
         </svg>
     );
